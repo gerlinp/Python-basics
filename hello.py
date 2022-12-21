447ed3f6ec027ea3ee69208fbc9ad55e0d066100
Gerlin <68314210+gerlinp@users.noreply.github.com>
2022-12-20 23:47:48 -0500

    Types and Branching

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
 first_name = input("What is your first name?   ")
 
-print(first_name)
 print("Hello", first_name)
-print(first_name, "is learning python")
+if first_name == "Gerlin":
+    print(first_name, "is learning python")
+elif first_name == "Steven":
+    print(first_name, "is learning with fellow students in the Community! Me too!")
+else:
+    #This is just in case we have a younger user who can't yet read
+    age = int(input("How old are you?  "))
+    if age <= 6:
+        print("Wow if you're {}! If you're confident with your reading already...".format(age)) 
+    print("You should totally learn Python, {}!".format(first_name))
+print("Have a great day {}!".format(first_name))
+
