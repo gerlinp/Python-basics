dddcb6f1c2d582b4a13a35cfab34edac8727fd58
Gerlin <68314210+gerlinp@users.noreply.github.com>
2022-12-22 01:59:34 -0500

    packing example

---
+++ LISTS/cart.py
@@ -0,0 +1,10 @@
+def calculate_total(*args):
+    total = sum(args)
+    print(total)
+
+# calculate_total(25,25,20,30)
+
+def packer(*args):
+    print("These are the characters I plan to try in SF6:")
+    print(args)
+packer("Kimberly", "DeeJay", "Marisa", "Jamie")
