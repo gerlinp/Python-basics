e0a11752281f1402961631ebb4238f6e6724728c
Gerlin <68314210+gerlinp@users.noreply.github.com>
2022-12-22 23:34:11 -0500

    Update testing.py

--- SEQUENCES/testing.py
+++ SEQUENCES/testing.py
@@ -1,6 +1,4 @@
-docs = 'Tuples are immutable sequences, typically used to store collections of heterogenous data (such as the 2-tuples produced by the enumerate() built-in). Tuples are also used for cases where an immutable sequence of homogeneous data is needed (such as allowing storage in a set or dict instance).'
+teachers = ['Alena','Ashley','Nicole','Treasure']
 
-if 'tuple' not in docs:
-    print('tuple is here')
-else:
-    print('tuple is not here')
+print(teachers.index('Nicole'))
+
