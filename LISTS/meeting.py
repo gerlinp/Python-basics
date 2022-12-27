5d82dbb399f147717afafd97d927f867bd6dcaf7
Gerlin <68314210+gerlinp@users.noreply.github.com>
2022-12-26 23:42:49 -0500

    getting there

--- LISTS/meeting.py
+++ LISTS/meeting.py
@@ -1,2 +1,6 @@
 attendees = ["Ken", "Alena", "Treasure"]
-print("There are", len(attendees), "attendees currently")
+attendees.append("Ashley")
+attendees.extend(["James", "Guil"])
+optional_invitees = ["Ben J,", "Dave"]
+potential_attendees = attendees + optional_invitees
+print("There are", len(potential_attendees), "potential attendees currently")
